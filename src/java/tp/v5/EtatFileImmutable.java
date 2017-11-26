50877d60e5dd74748195366d130b6cc54cc86a16
diabou <diabmaaloufa@gmail.com>
2017-11-26 18:30:15 +0100

    Javadoc EtatFileImmutable

--- src/java/tp/v5/EtatFileImmutable.java
+++ src/java/tp/v5/EtatFileImmutable.java
@@ -2,16 +2,28 @@ package tp.v5;
 
 /**
  * Created by francoismilhem on 22/11/2017.
+ * @param <E> Le type des éléments de la file.
  */
 public interface EtatFileImmutable<E>  extends File<EtatFileImmutable<E>, E>, IdentifiableParIteration<FileImmutable<E>, E>, RepresentableParIteration<E>  {
 
+    /**
+     * @param dernier
+     * @return la file this ajoutée de l'élément en paramètre, en dernière position.
+     */
     EtatFileImmutable<E> creer(E dernier);
 
+    /**
+     * @return la file this sans son premier élément.
+     */
     default EtatFileImmutable<E> retrait(){
 
         return this.suivants();
     }
 
+    /**
+     * @param dernier
+     * @return la file this ajoutée de l'élément en paramètre, en dernière position.
+     */
     default EtatFileImmutable<E> ajout(E dernier){
 
         return this.creer(dernier);
