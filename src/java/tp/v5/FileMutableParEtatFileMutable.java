93fbc9ac8285419ab0cb63d9e7375a77060facb9
diabou <diabmaaloufa@gmail.com>
2017-11-26 18:54:17 +0100

    Javadoc FileMutableParEtatFileMutable

--- src/java/tp/v5/FileMutableParEtatFileMutable.java
+++ src/java/tp/v5/FileMutableParEtatFileMutable.java
@@ -1,48 +1,72 @@
 package tp.v5;
 
-import java.util.Iterator;
 
 /**
  * Created by francoismilhem on 22/11/2017.
+ * Classe implémentant une file mutable par l'état d'une file mutable.
+ * @param <E> le type des éléments de la file.
  */
 public class FileMutableParEtatFileMutable<E>  {
 
     private EtatFileMutable<E> etatFileMutable;
 
+    /**
+     * Constructeur pour une file non vide.
+     *
+     * @param etatFileMutable la file représentant this.
+     */
     public FileMutableParEtatFileMutable(EtatFileMutable etatFileMutable){
-
 
         this.etatFileMutable = etatFileMutable;
     }
 
+    /**
+     * @return le premier élément de this.
+     */
     public E premier(){
 
         return etatFileMutable.premier();
     }
 
+    /**
+     * @return this sans son premier élément.
+     */
     public EtatFileMutable suivants(){
 
         return etatFileMutable.suivants();
     }
 
+    /**
+     * Fabrique.
+     * @return une file vide.
+     */
     public EtatFileMutable creer(){
 
         return etatFileMutable.creer();
     }
 
-
+    /**
+     * @param dernierDansFile
+     * @return une copie de this ajoutée de l'élément en paramètre, en dernière position.
+     */
     public EtatFileMutable ajout(E dernierDansFile){
 
         this.etatFileMutable =  etatFileMutable.ajout(dernierDansFile);
         return this.etatFileMutable;
     }
 
+    /**
+     * @return une copie de this sans son premier élément.
+     */
     public EtatFileMutable retrait(){
 
         this.etatFileMutable =  etatFileMutable.retrait();
         return this.etatFileMutable;
     }
 
+    /**
+     * @return le nombre d'éléments que content this.
+     */
     public int taille(){
 
         return etatFileMutable.taille();
