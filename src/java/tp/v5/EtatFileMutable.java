64669b986cb268f9a8976218bb174e799d79ff82
diabou <diabmaaloufa@gmail.com>
2017-11-26 18:36:13 +0100

    Javadoc EtatFileMutable

--- src/java/tp/v5/EtatFileMutable.java
+++ src/java/tp/v5/EtatFileMutable.java
@@ -2,19 +2,28 @@ package tp.v5;
 
 /**
  * Created by francoismilhem on 22/11/2017.
+ * @param <E> Le type des éléments de la file mutable.
  */
 public interface EtatFileMutable<E> extends EtatFile<EtatFileMutable<E>, E>, IdentifiableParIteration<FileMutable<E>, E>, RepresentableParIteration<E>  {
 
+    /**
+     * Ajoute à this l'élément en paramètre.
+     *
+     * @param element
+     */
     default void ajouter(E element){
-
         this.ajout(element);
     }
 
+    /**
+     * Retire à this son premier élément.
+     */
     default void retirer(){
 
         this.retirer();
     }
 
+    @Override
     default EtatFileMutable<E> suivants(){
 
         EtatFileMutable<E> copie = this.creerCopie();
@@ -22,6 +31,9 @@ public interface EtatFileMutable<E> extends EtatFile<EtatFileMutable<E>, E>, Ide
         return copie ;
     }
 
+    /**
+     * @return Une copie de this.
+     */
     EtatFileMutable<E> creerCopie();
 
 }
