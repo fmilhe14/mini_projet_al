47b721c0ae2e03c55fd78c5fc205533baf227719
diabou <diabmaaloufa@gmail.com>
2017-11-26 18:51:07 +0100

    Javadoc FileMutable

--- src/java/tp/v5/FileMutable.java
+++ src/java/tp/v5/FileMutable.java
@@ -2,21 +2,37 @@ package tp.v5;
 
 /**
  * Created by francoismilhem on 22/11/2017.
+ * Interface représentant les propriétés d'une file mutable.
+ * @param <E> le type des éléments de la file.
  */
 public interface FileMutable<E> extends File<FileMutable<E>, E>, IdentifiableParIteration<FileMutable<E>, E>, RepresentableParIteration<E> {
 
+    /**
+     * Ajoute l'élément en paramètre à la file this.
+     *
+     * @param element
+     */
     default void ajouter(E element){
-
         this.ajout(element);
     }
 
+    /**
+     * Contatène la file en paramètre à this.
+     * @param file
+     */
     void ajouter(FileMutable<E> file);
 
+    /**
+     * Retière la tête de this.
+     */
     default void retirer(){
 
         this.retirer();
     }
 
+    /**
+     * @return une liste contenant tous les éléments de this sauf la tête.
+     */
     default FileMutable<E> suivants(){
 
         FileMutable<E> copie = this.creerCopie();
@@ -24,6 +40,10 @@ public interface FileMutable<E> extends File<FileMutable<E>, E>, IdentifiablePar
         return copie ;
     }
 
+    /**
+     * Fabrique.
+     * @return une copie de this.
+     */
     FileMutable<E> creerCopie();
 
 }
