33c5e70421680ad61c1c59ce8c8f7281f154142f
diabou <diabmaaloufa@gmail.com>
2017-11-26 18:41:47 +0100

    Javadoc File

--- src/java/tp/v5/File.java
+++ src/java/tp/v5/File.java
@@ -2,18 +2,35 @@ package tp.v5;
 
 /**
  * Created by francoismilhem on 22/11/2017.
+ * Interface représentant un cas général de file.
+ * @param <K> Le type de la file.
+ * @param <E> Le type des éléments de la file.
  */
 public interface File<K extends File<K, E>, E> extends Iterable<E>, Mesurable  {
 
-
+    /**
+     * @return le premier élément de this.
+     */
     E premier();
+
+    /**
+     * @return la file this sans son premier élément.
+     */
     K suivants();
 
+    /**
+     * @return true ssi this est vide.
+     */
     default boolean estVide(){
 
         return this.taille() == 0 ;
     }
 
+    /**
+     * Fabrique.
+     *
+     * @return une file vide.
+     */
     K creer();
 
     @SuppressWarnings("unchecked")
@@ -21,11 +38,22 @@ public interface File<K extends File<K, E>, E> extends Iterable<E>, Mesurable  {
         return (K)this;
     }
 
+    /**
+     * @param dernierDansFile
+     * @return une copie de this ajoutée de l'élément en paramètre, en dernière position.
+     */
     K ajout(E dernierDansFile);
 
+    /**
+     * @return une copie de this sans son premier élément.
+     */
     K retrait();
 
-    //Méthode recupérée dans la classe v3.File
+    /**
+     * Méthode recupérée dans la classe v3.File
+     * @param secondeFile
+     * @return une copie de this concaténée avec la file en paramètre.
+     */
     default K ajout(K secondeFile){
 
         K r = this.sujet();
