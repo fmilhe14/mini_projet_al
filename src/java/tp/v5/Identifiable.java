88a5007f7b28509e7a2bf434209a4adc33f93a7a
diabou <diabmaaloufa@gmail.com>
2017-11-26 18:57:19 +0100

    Javadoc Identfiable et IdentifiableParItération

--- src/java/tp/v5/Identifiable.java
+++ src/java/tp/v5/Identifiable.java
@@ -1,7 +1,16 @@
 package tp.v5;
 
+/**
+ * Interface représentant un élément identifiable.
+ *
+ * @param <K> le type de l'élément ayant la propriété d'être identifiable.
+ */
 public interface Identifiable<K> {
 
-	boolean estEgal(K autre);
+    /**
+     * @param autre
+     * @return true si les éléments de this sont les mêmes que ceux de la liste donnée en paramètre.
+     */
+    boolean estEgal(K autre);
 
 }
